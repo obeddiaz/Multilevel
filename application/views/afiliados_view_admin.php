1e3bff66b99b194cf71e28c664d029e422fcfbd8
Obed Díaz <obed.diaz.r@gmail.com>
2014-10-05 12:41:28 -0500

    añadir archivos no versionados

---
+++ application/views/afiliados_view_admin.php
@@ -0,0 +1,20 @@
+<?php for ($a = 1; $a <= $niveles; $a++) { ?>
+    <table id="tableEdo" ng-table="tableParams" class="table table-condensed table-hover">
+        <thead>
+            <tr>
+                <th>Nombre</th>
+                <th>Apellido paterno</th>
+                <th>Apellido Materno</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach (${"afiliados$a"} as $afi): ?>
+                <tr>
+                    <td><?php echo $afi["nombre"]; ?></td>
+                    <td><?php echo $afi["apellido_paterno"]; ?></td>
+                    <td><?php echo $afi["apellido_materno"] ?></td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+<?php } ?>
